1b14be3cdea41c294fe44c9fb0db72a53b7e5c38
svdrski <svdrski@gmail.com>
2023-11-01 16:50:01 +0200

    ffsaf

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -3,7 +3,7 @@ import Card from './Card'
 
 
 const CardList = ({Robots}) => {
-    throw new Error('NOOO')
+    // throw new Error('NOOO')
 
     const cardComponent = Robots.map(item =>(
         <Card key={item.id}  id={item.id} name={item.name} email={item.email}/>
